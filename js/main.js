8d501f1d62e66dd7b690c3ea5bb812355a302009
andrea valero <estefania2002valero@gmail.com>
2024-02-26 22:35:53 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// Juego 
+
+
+ 
+ let snake=20;
+ let food=20;
+ let round=0;
+
+while(snake >0 && food >0){
+    round=round+1; round +=1;
+     console.log ("snake"+ snake)
+     console.log ("food"+ food)
+    console.log ("---round---"+ round)
+
+} 
+const MIN= 10;
+const MAX= 20;
+
+let velocidad=parseint (math.random ()* (Imax-min) + min)
+let foodT= parseint(math.random()* (max-min) +min)
+ if(food > snake){
+    console.log("Perdise intenta de nuevo")
+
+ }else if (snake => food){
+    console.log ("Ganaste juega de nuevo")
+
+ }
+
+
+ 
