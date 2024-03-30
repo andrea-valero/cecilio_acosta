996791f7beaf7b669a81353418e8f943b5d87e81
andrea valero <estefania2002valero@gmail.com>
2024-03-30 10:54:32 -0300

    sass

--- js/main.js
+++ js/main.js
@@ -1,3 +1,40 @@
+   // formato usario
+
+   let estudiantes = [];
+
+   document.getElementById('formulario').addEventListener('submit', function(event) {
+       event.preventDefault();
+       let nombre = document.getElementById('nombre').value;
+       let calificacion = parseFloat(document.getElementById('calificacion').value);
+       agregarEstudiante(nombre, calificacion);
+       asignarSeccion();
+       mostrarResultado();
+       document.getElementById('formulario').reset();
+   });
+
+   function agregarEstudiante(nombre, calificacion) {
+       estudiantes.push({ nombre: nombre, calificacion: calificacion });
+   }
+
+   function asignarSeccion() {
+    estudiantes.forEach(estudiante => {
+        estudiante.seccion = estudiante.calificacion >= 70 ? ' Seccion A' : ' Seccion B';
+    });
+}
+
+   function mostrarResultado() {
+       let resultadoDiv = document.getElementById('resultado');
+       resultadoDiv.innerHTML = '';
+       estudiantes.forEach(estudiante => {
+          resultadoDiv.innerHTML = estudiante.seccion 
+   })
+   
+  }
+   let estudiantesJOSN=JSON.stringify(estudiantes);
+   localStorage.setItem( "estudiantes1",estudiantesJOSN);
+
+
+
 
 function saludar(){
     alert(" Bienvenido recoger los horarios esta semana")
@@ -64,40 +101,6 @@ const btn=document.querySelector('#btn');
     let cvv = document.getElementById('cvv').value;
    let titular = document.getElementById('titular').value;
    })
-   // formato usario
-
-   let estudiantes = [];
-
-   document.getElementById('formulario').addEventListener('submit', function(event) {
-       event.preventDefault();
-       let nombre = document.getElementById('nombre').value;
-       let calificacion = parseFloat(document.getElementById('calificacion').value);
-       agregarEstudiante(nombre, calificacion);
-       asignarSeccion();
-       mostrarResultado();
-       document.getElementById('formulario').reset();
-   });
-
-   function agregarEstudiante(nombre, calificacion) {
-       estudiantes.push({ nombre: nombre, calificacion: calificacion });
-   }
-
-   function asignarSeccion() {
-    estudiantes.forEach(estudiante => {
-        estudiante.seccion = estudiante.calificacion >= 70 ? ' Seccion A' : ' Seccion B';
-    });
-}
-
-   function mostrarResultado() {
-       let resultadoDiv = document.getElementById('resultado');
-       resultadoDiv.innerHTML = '';
-       estudiantes.forEach(estudiante => {
-          resultadoDiv.innerHTML = estudiante.seccion 
-   })
-   
-  }
-  
-
 
   
  
