9097b3588beebd226d55595c8f0d18518627cba7
andrea valero <estefania2002valero@gmail.com>
2024-04-18 21:07:44 -0300

    node libreria asincronica

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
-   // formato usario
 
 
-
-   let estudiantes = [];
+let estudiantes = [];
 
    document.getElementById('formulario').addEventListener('submit', function(event) {
        event.preventDefault();
@@ -35,105 +33,44 @@ function mostrarResultado() {
         resultadoDiv.innerHTML = estudiante.seccion
     });
 }
-   mostrarResultado()
-   
-  
-const estudiantesGuardados = localStorage.getItem('estudiantes');
 
-
-if (estudiantesGuardados) {
- 
-  const estudiantes = JSON.parse(estudiantesGuardados);
-
-  
-  console.log(estudiantes);
-} else {
-  console.log('No hay datos de estudiantes guardados en localStorage.');
-}
-
-    
-
- 
-
-for(let i=0; i<5; i+=1 ){
-   console.log("llena formulario")
-
-};
-
-let cuotas ={
-    matricula:100000,
-    colegiatura:200000,
-    tiempodepago: "trimestral",
-
-   
- }
- console.table (cuotas)
- // semestre
-
- const meses1=["Enero", "Febrero","marzo"]
- const meses2=["Abril","Mayo","Junio"]
- const semestre=[...meses1, ...meses2 ]
-  console.log ( semestre)
-  console.log(typeof semestre)
-  semestre.push("Julio");
-  semestre.map
- 
- 
-
-// Descuento 20
-const calcular20porciento= numero=> numero * 0.8;
-const matricula=100000;
-const resultado=calcular20porciento(matricula);
-console.log(resultado);
-
-
-//inscripciones
-
-let inscripcion= [1,2,3,4]
-
-let dobleinscripcion= inscripcion.map(num =>num *2)
-console.log(dobleinscripcion)
-
-
-
-//agregar las materias
-
+mostrarResultado()   
 
 const btn=document.querySelector('#btn');
  const materias=document.querySelector('#materias');
   btn.onclick=function(){
   materias.innerHTML="Materias agregadas";
 }
- setTimeout(function(click,btn){
+ setTimeout(()=>{
 
   materias.innerHTML="Pasa por tu horario"
 
  },7000)
 
+ Swal.fire("Bienvenido a nuestra pagina");
+ 
 
- 
- 
-  document.getElementById('formulario').addEventListener('paga', function(event) {
-    event.preventDefault();
-    
-    let numerodetarjeta= document.getElementById('numerodetarjeta').value;
-    let vencimieto = document.getElementById('vencimiento').value;
-    let cvv = document.getElementById('cvv').value;
-   let titular = document.getElementById('titular').value;
-   });
-
-   //libreria
-   Swal.fire("Bienvenido");
-    
-   
-   
- 
-   fetch("alumnos.json")
+ fetch('../data/alumno.json')
   .then(response => response.json())
   .then(data => {
-    console.log(data);
+    mostrarDatosEnPagina(alumno.json);
   })
   .catch(error => {
-    console.error('Error al obtener los datos de los alumnos:', error);
+    datoscolegio.innerHTML('Hubo un error');
   });
-   
+
+function mostrarDatosEnPagina(data) {
+  const datosColegioElement = document.getElementById('datosColegio');
+  
+  
+  
+  const listaEstudiantes = document.createElement('ul');
+  data.forEach(estudiante => {
+    const listItem = document.createElement('li');
+    listItem.textContent = "${estudiante.nombre} - ${estudiante.edad}";
+    listaEstudiantes.appendChild(listItem);
+  });
+  
+  // Añadir la lista al elemento en la página
+  datosColegioElement.appendChild(listaEstudiantes);
+}
